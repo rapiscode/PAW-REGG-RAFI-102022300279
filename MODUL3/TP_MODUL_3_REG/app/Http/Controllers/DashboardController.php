c090ce56a634da7266fed0af3635f88356f9c8ab
Naufal Rafi <naufalnr19@gmail.com>
2025-04-19 22:27:41 +0700

    TP MODUL 3

---
+++ MODUL3/TP_MODUL_3_REG/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        // ==================2==================
+        // - Set timezone ke Asia/Jakarta
+        date_default_timezone_set('Asia/Jakarta');
+        // - Buat variabel nama, jam, waktu
+        $nama = "Naufal Rafi";
+        $jam = date('H:i:s');
+        $waktu = date('H');
+        // - Tentukan $salam berdasarkan jam (pagi, siang, sore, malam)
+        if ($waktu >= 5 && $waktu < 12) {
+            $salam = "Selamat Pagi";
+        } elseif ($waktu >= 12 && $waktu < 15) {
+            $salam = "Selamat Siang";
+        } elseif ($waktu >= 15 && $waktu < 18) {
+            $salam = "Selamat Sore";
+        } else {
+            $salam = "Selamat Malam";
+        }
+        // - Panggil fungsi getTanggal()
+        $tanggal = $this->getTanggal();
+        // - Kirim data ke view 'dashboard' 
+        return view('dashboard', compact('salam', 'nama', 'jam', 'tanggal'));
+    }
+
+    private function getTanggal()
+    {
+        // ==================3==================
+        // - Kembalikan tanggal sekarang dalam format dd-mm-yyyy
+        return date('d-m-Y');
+    }
+}
